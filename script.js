228c835f244bd18f83d99f1e1b7b688ac037f714
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-03-08 20:15:26 -0300

    entendendo dimensãoes e distancia

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const listaAnimais = document.querySelector('.animais-lista');
+
+const height = listaAnimais.scrollheight;
+const animaisTop = listaAnimais.offsetTop;
+console.log(animaisTop);
+
+const primeiroh2 = document.querySelector('h2');
+const h2left = primeiroh2.offsetLeft
+
+const rect = primeiroh2.getBoundingClientRect();
+
+console.log(rect.top);
+
+if(rect.top < 0) {
+  console.log('passou do elemento')
+}
+
+console.log (
+  window.innerWidth,
+  window.innerHeight,
+  window.outerWidth,
+  window.outerHeight,
+  window.pageYOffset,
+);
+
+
+const small = window.matchMedia('(max-width: 600px)').matches;
+
+if(small) {
+  console.log('Usuário mobile');
+} else {
+  console.log('Usuário desktop');
+}
 
