66049afdf37ec0a3d06e0fcc84330560839491bf
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-03-22 20:01:35 -0300

    scroll suave

--- script.js
+++ script.js
@@ -41,13 +41,42 @@ function initAccordion() {
 }
 initAccordion();
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget;
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
 }
+initAccordion();
 
-linksInternos.forEach(() => {
-link.addEventListener('click', scrollToSection);
-});
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+    
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
