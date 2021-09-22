2a05075ec928fd2f5d5b58a45118a0a396312517
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2021-09-22 08:56:06 -0300

    dropdown-menu implemented

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initmodal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -11,3 +12,4 @@ initAccordion();
 initTabNav();
 initmodal();
 initTooltip();
+initDropdownMenu();
