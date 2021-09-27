b332437168742898df4be6a22021f614aeb4a931
Guilherme-mtk <guilhermejesuslima32@gmail.com>
2021-09-27 09:31:27 -0300

    added bitcoin donations

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -18,4 +20,6 @@ initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
-initFuncionamento;
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
